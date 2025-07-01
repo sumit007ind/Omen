de4937a7b7c60c53e6da6760c27ecce559931fc6
sumit007ind <sharmaraj81808@gmail.com>
2025-07-01 22:06:00 +0530

    created new project

---
+++ OmenList/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full  h-screen  text-black'>
+      Hello Sumit sharma 
+      <div className=''></div>
+    </div>
+  )
+}
+
+export default App
