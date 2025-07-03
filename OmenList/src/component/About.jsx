19fe18c379701a4946a8a080c712d9a1f12ff4db
sumit007ind <sharmaraj81808@gmail.com>
2025-07-04 02:40:25 +0530

    iljo

--- OmenList/src/component/About.jsx
+++ OmenList/src/component/About.jsx
@@ -9,8 +9,9 @@ const About = () => {
       </h1>
       <div className='w-full flex gap-5 pt-15 mt-20 border-t-[3px] border-[#a1b562]'>
         <div className='w-1/2 '>
+
         <h1 className='text-7xl'>Our approach:</h1>
-        <button className=' hover:bg-black flex items-center gap-5 px-10 mt-8 font-semibold py-1 bg-zinc-900 rounded-full text-white '>Read More
+        <button className=' hover:bg-black   flex items-center gap-5 px-10 mt-8 font-semibold py-1 bg-zinc-900 rounded-full text-white '>Read More
             <div className='w-4 h-4 bg-zinc-100 rounded-full'></div>
         </button>
         </div>
